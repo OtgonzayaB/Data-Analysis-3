51aeba8c700e43b07cc66436e61a4e6c772dba47
OtgonzayaB <otgonzaya6052@gmail.com>
2025-03-02 21:14:01 +0100

    Model Estimations

--- assignment-01.R
+++ assignment-01.R
@@ -51,6 +51,62 @@ print(nzv_vars[nzv_vars$nzv, ])  # Show problematic variables
 # Drop near-zero variance variables
 pharmacists <- pharmacists[, !names(pharmacists) %in% rownames(nzv_vars[nzv_vars$nzv, ])]
 
+####### MODEL ESTIMATIONS #######
+
+##### Model 1: Baseline (Age & Sex)
+model1 <- lm(earn_per_hour ~ age + sex, data = pharmacists)
+
+summary(model1)
+
+##### Model 2: Add Education & Employment Class
+
+pharmacists <- droplevels(subset(pharmacists, class != "Government - Local"))
+
+model2 <- lm(earn_per_hour ~ age + sex + grade92 + class, data = pharmacists)
+
+summary(model2)
+
+##### Model 3: Add State, Citizenship, and Industry
+
+pharmacists <- pharmacists[, !names(pharmacists) %in% c("region")]
+
+
+pharmacists$prcitshp <- factor(ifelse(pharmacists$prcitshp %in% c("Native, Born Abroad Of US Parent(s)", 
+                                                                  "Native, Born in PR or US Outlying Area"), 
+                                      "Other", pharmacists$prcitshp))
+
+model3 <- lm(earn_per_hour ~ age + sex + grade92 + class + prcitshp, data = pharmacists)
+
+summary(model3)
+
+#####  Model 4: Optimized Model
+# Selectively add only the most significant industry categories
+
+selected_industries <- c("Grocery stores (4451)", "Justice, public order, and safety activities (922, pt. 92115)",  
+                         "State 92", "State 82", "Pharmacies and drug stores (44611)")
+
+pharmacists_filtered <- pharmacists[pharmacists$ind02 %in% selected_industries, ]
+
+# Convert categorical variables
+pharmacists_filtered$ind02 <- as.factor(pharmacists_filtered$ind02)
+pharmacists_filtered$class <- as.factor(pharmacists_filtered$class)
+pharmacists_filtered$sex <- as.factor(pharmacists_filtered$sex)
+
+# Create dummy for "Grocery Stores"
+pharmacists_filtered$ind02_GroceryStores <- ifelse(pharmacists_filtered$ind02 == "Grocery stores (4451)", 1, 0)
+
+# Center age to reduce collinearity
+pharmacists_filtered$age_centered <- pharmacists_filtered$age - mean(pharmacists_filtered$age, na.rm = TRUE)
+pharmacists_filtered$age2_centered <- pharmacists_filtered$age_centered^2
+
+# Drop categories with insufficient observations
+pharmacists_filtered <- droplevels(subset(pharmacists_filtered, class != "Government - Local"))
+pharmacists_filtered <- subset(pharmacists_filtered, ind02_GroceryStores != 1)
+
+# Model 4 Final
+model4 <- lm(earn_per_hour ~ age_centered + age2_centered + sex + grade92 + class, data = pharmacists_filtered)
+
+summary(model4)
 
 
 
